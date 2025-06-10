50b481ababe3e974c6b0f885abaaf719c4920211
sultann <manikdrmc@gmail.com>
2025-06-10 20:48:41 +0600

    Update version to 2.0.7

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 /**
  * External dependencies
  */
-const WebpackBar = require('webpackbar');
-const MomentTimezoneDataPlugin = require('moment-timezone-data-webpack-plugin');
-const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const path = require('path');
+const WebpackBar = require( 'webpackbar' );
+const MomentTimezoneDataPlugin = require( 'moment-timezone-data-webpack-plugin' );
+const RemoveEmptyScriptsPlugin = require( 'webpack-remove-empty-scripts' );
+const CopyWebpackPlugin = require( 'copy-webpack-plugin' );
+const path = require( 'path' );
 /**
  * Internal dependencies
  */
-const { globFiles, hasArgInCLI, getArgFromCLI } = require('./utils');
+const { globFiles, hasArgInCLI, getArgFromCLI } = require( './utils' );
 
 // This must go before calling `createConfig` to ensure the environment variable is set.
 process.env.WP_SOURCE_PATH = process.env.WP_SOURCE_PATH || 'resources';
@@ -21,85 +21,91 @@ process.env.WP_SOURCE_PATH = process.env.WP_SOURCE_PATH || 'resources';
  * @param {Object|Function} files      - List of JS or CSS/SCSS entry files.
  * @return {Object} Final Webpack configuration.
  */
-function createConfig(baseConfig, files = (x) => x) {
-	if (!baseConfig || typeof baseConfig !== 'object') {
+function createConfig( baseConfig, files = ( x ) => x ) {
+	if ( ! baseConfig || typeof baseConfig !== 'object' ) {
 		throw new Error(
 			'A valid @wordpress/scripts config must be passed as the first argument.'
 		);
 	}
 
-	const output = hasArgInCLI('--output-path')
-		? getArgFromCLI('--output-path')
+	const output = hasArgInCLI( '--output-path' )
+		? getArgFromCLI( '--output-path' )
 		: 'assets';
 	const source = process.env.WP_SOURCE_PATH;
-	const sourcePath = path.resolve(process.cwd(), source);
-	const outputPath = path.resolve(process.cwd(), output);
+	const sourcePath = path.resolve( process.cwd(), source );
+	const outputPath = path.resolve( process.cwd(), output );
 
 	// Allow copying PHP files to the dist directory.
 	process.env.WP_COPY_PHP_FILES_TO_DIST = true;
 
-	const getFiles = () => ({
-		...globFiles(sourcePath, [
+	const getFiles = () => ( {
+		...globFiles( sourcePath, [
 			'{scripts,styles}/*/index.{js,jsx,ts}',
 			'{scripts,styles}/*/!(_)*.{js,jsx,ts,scss,sass,css}',
-		]).reduce((entries, file) => {
-			const [, type, domain, filename] =
-				file.match(new RegExp(`${source}/([^/]+)\/([^/]+)\/([^/]+)`)) ||
-				[];
-			if (!type || !domain || !filename) {
+		] ).reduce( ( entries, file ) => {
+			const [ , type, domain, filename ] =
+			file.match( new RegExp( `${ source }/([^/]+)\/([^/]+)\/([^/]+)` ) ) ||
+			[];
+			if ( ! type || ! domain || ! filename ) {
 				return entries;
 			}
 
-			const name = path.basename(filename, path.extname(filename));
+			const name = path.basename( filename, path.extname( filename ) );
 			entries[
-				`${type}/${domain}-${name}`
+				`${ type }/${ domain }-${ name }`
 					.replace(
-						new RegExp(`^${type}/(?!admin-|frontend-)([^-]+)-`),
-						`${type}/`
+						new RegExp( `^${ type }/(?!admin-|frontend-)([^-]+)-` ),
+						`${ type }/`
 					)
-					.replace(new RegExp(`\\b(${domain})-\\1\\b`), '$1')
-			] = path.resolve(file);
+					.replace( new RegExp( `\\b(${ domain })-\\1\\b` ), '$1' )
+				] = path.resolve( file );
 			return entries;
-		}, {}),
-		...globFiles(sourcePath, [
+		}, {} ),
+		...globFiles( sourcePath, [
+			'client/index.{js,jsx,ts}',
 			'client/*/index.{js,jsx,ts}',
-			'client/*/*/index.{js,jsx,ts}',
-		]).reduce((entries, file) => {
+		] ).reduce( ( entries, file ) => {
 			const match = file.match(
 				new RegExp(
-					`${source}/client/(.+?)\/(?:([^/]+)\/)?index\\.(js|jsx|ts)$`
+					`${ source }/client/(?:([^/]+)/)?index\\.(js|jsx|ts)$`
 				)
 			);
-			if (!match) {
-				return entries;
-			}
 
-			const [, type, name = type] = match;
-			entries[
-				`client/${type}-${name}`
-					.replace(/^client\/(?!admin-|frontend-)([^-]+)-/, 'client/')
-					.replace(new RegExp(`\\b(${type})-\\1\\b`), '$1')
-			] = path.resolve(file);
+			const [ , type ] = match;
+			const entryName = type || 'index';
+			entries[ `client/${ entryName }` ] = path.resolve( file );
 			return entries;
-		}, {}),
-	});
+		}, {} ),
+	} );
 
 	return {
 		...baseConfig,
 		entry: {
-			...(typeof baseConfig.entry === 'function'
+			...( typeof baseConfig.entry === 'function'
 				? baseConfig.entry()
-				: baseConfig.entry),
-			...((typeof files === 'function' ? files(getFiles()) : files) ||
-				{}),
+				: baseConfig.entry ),
+			...( ( typeof files === 'function' ? files( getFiles() ) : files ) ||
+				{} ),
 		},
 		output: {
 			...baseConfig.output,
 			path: outputPath,
+			chunkFilename: 'chunks/[name].js',
 		},
 		resolve: {
 			...baseConfig.resolve,
-			modules: [path.resolve(process.cwd(), 'node_modules')],
+			modules: [ path.resolve( process.cwd(), 'node_modules' ) ],
+			alias: {
+				...baseConfig.resolve.alias,
+				'@components': path.resolve( process.cwd(), `${ source }/client/shared/components` ),
+				'@utils': path.resolve( process.cwd(), `${ source }/client/shared/utils` ),
+			},
+		},
+		externals: {
+			...baseConfig.externals,
+			lodash: 'lodash',
+			jquery: 'jQuery',
+			$: 'jQuery',
 		},
 		stats: {
 			all: false,
@@ -119,17 +125,17 @@ function createConfig(baseConfig, files = (x) => x) {
 			 *
 			 * @see https://www.npmjs.com/package/moment-timezone-data-webpack-plugin
 			 */
-			new MomentTimezoneDataPlugin({
+			new MomentTimezoneDataPlugin( {
 				// This strips out timezone data before the year 2000 to make a smaller file.
 				startYear: 2000,
-			}),
+			} ),
 
 			/**
 			 * Copy source files/directories to a build directory.
 			 *
 			 * @see https://www.npmjs.com/package/copy-webpack-plugin
 			 */
-			new CopyWebpackPlugin({
+			new CopyWebpackPlugin( {
 				patterns: [
 					{
 						from: 'images/**/*.{jpg,jpeg,png,gif,svg}',
@@ -144,17 +150,17 @@ function createConfig(baseConfig, files = (x) => x) {
 						noErrorOnMissing: true,
 					},
 				],
-			}),
+			} ),
 
 			/**
 			 * Remove empty scripts.
 			 *
 			 * @see https://www.npmjs.com/package/webpack-remove-empty-scripts
 			 */
-			new RemoveEmptyScriptsPlugin({
+			new RemoveEmptyScriptsPlugin( {
 				stage: RemoveEmptyScriptsPlugin.STAGE_AFTER_PROCESS_PLUGINS,
 				remove: /\.(js)$/,
-			}),
+			} ),
 
 			/**
 			 * Show progressbar for cleaner build output.
