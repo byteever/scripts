aef300ce4f057051e751bff806b1fd2a195bc24c
sultann <manikdrmc@gmail.com>
2025-06-14 21:01:04 +0600

    Release v2.0.8

--- bin/release.sh
+++ bin/release.sh
@@ -8,20 +8,13 @@ echo "➤ Preparing release for $VERSION..."
 read -p "Are you sure you want to release version $VERSION? (y/n) " -n 1 -r
 echo ""
 
-# Push the changes to the repository.
-echo "➤ Pushing changes to the repository..."
-git add .
-git commit -m "Update version to $VERSION"
-git push origin master
-echo "✓ Changes pushed to the repository!"
-
-# create a github release
-echo "➤ Creating GitHub release..."
+# Push the changes to the repository and create release.
+echo "➤ Pushing changes and creating GitHub release..."
 git add .
 git commit -m "Release v$VERSION"
 git tag -a "v$VERSION" -m "Release v$VERSION"
 git push origin master --tags
-echo "✓ GitHub release created!"
+echo "✓ Changes pushed and GitHub release created!"
 
 # Release the package to npm.
 echo "➤ Releasing package to npm..."
